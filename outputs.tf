1e060ef1f3b04552dca26924533b4060601ae8af
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "container_registry_agent_pools" {
-  description = "All container_registry_agent_pool resources"
-  value       = azurerm_container_registry_agent_pool.container_registry_agent_pools
-}
 output "container_registry_agent_pools_container_registry_name" {
-  description = "List of container_registry_name values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.container_registry_name]
+  description = "Map of container_registry_name values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.container_registry_name }
 }
 output "container_registry_agent_pools_instance_count" {
-  description = "List of instance_count values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.instance_count]
+  description = "Map of instance_count values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.instance_count }
 }
 output "container_registry_agent_pools_location" {
-  description = "List of location values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.location]
+  description = "Map of location values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.location }
 }
 output "container_registry_agent_pools_name" {
-  description = "List of name values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.name]
+  description = "Map of name values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.name }
 }
 output "container_registry_agent_pools_resource_group_name" {
-  description = "List of resource_group_name values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.resource_group_name]
+  description = "Map of resource_group_name values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.resource_group_name }
 }
 output "container_registry_agent_pools_tags" {
-  description = "List of tags values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.tags]
+  description = "Map of tags values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.tags }
 }
 output "container_registry_agent_pools_tier" {
-  description = "List of tier values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.tier]
+  description = "Map of tier values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.tier }
 }
 output "container_registry_agent_pools_virtual_network_subnet_id" {
-  description = "List of virtual_network_subnet_id values across all container_registry_agent_pools"
-  value       = [for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : v.virtual_network_subnet_id]
+  description = "Map of virtual_network_subnet_id values across all container_registry_agent_pools, keyed the same as var.container_registry_agent_pools"
+  value       = { for k, v in azurerm_container_registry_agent_pool.container_registry_agent_pools : k => v.virtual_network_subnet_id }
 }
 
